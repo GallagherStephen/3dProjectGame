cc79d126e449038d4694368e6f61ec8947b57d06
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-03-11 14:21:06 +0000

    trigger at start added with objective + removed unnessesary terrain sg

---
+++ Assets/scripts/objectiveScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectiveScript : MonoBehaviour
+{
+    //===================================================
+    //Variables
+    //===================================================
+    public GameObject UiObject;
+    public GameObject cube;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(true);
+    }
+
+    //===================================================
+    // WHEN "player" ENTERS TRIGGER 
+    //====================================================
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            UiObject.SetActive(true);
+    }
+    //===================================================
+    // WHEN "player" EXITs TRIGGER 
+    //====================================================
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        UiObject.SetActive(false);
+        Destroy(cube);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
