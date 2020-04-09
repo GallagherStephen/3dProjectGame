4cb56d79ef9fc0e3386adccdef85edb61fc8d3c3
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-04-09 12:00:49 +0100

    added main menu and rules page

---
+++ Assets/scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private string nextlevel; //scene to move too!
+
+    public void ButtonMoveScene(string nextlevel)
+    {
+        SceneManager.LoadScene(nextlevel);
+    }
+}
