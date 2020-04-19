173cef4a38e345eafbe8897fe04dc183208ccb69
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-04-19 12:35:08 +0100

    added welcome tects to each part of map nd initial objective
    added welcome tects to each part of map nd initial objective

--- Assets/scripts/objectiveScript.cs
+++ Assets/scripts/objectiveScript.cs
@@ -14,7 +14,7 @@ public class objectiveScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UiObject.SetActive(true);
+        UiObject.SetActive(false);
     }
 
     //===================================================
