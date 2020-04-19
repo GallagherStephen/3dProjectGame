b9a8f49f661205f0f7c1f5eb1a6b0051e1089d07
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-04-19 15:31:36 +0100

    flag at castle to enter end game scene
    trigger at castle to enter new scene,spawns inside castle walls + flag at castle when touched ends game.

---
+++ Assets/scripts/moveScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveScene : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider col){
+        if (col.CompareTag("Player"))
+        {
+              SceneManager.LoadScene("EndGameScene");
+        }
+    }
+
+}
---
+++ Assets/scripts/moveSceneToCastle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveSceneToCastle : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("CastleScene");
+        }
+    }
+
+}
